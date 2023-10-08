3833e60df2b2880cffa52e33b036239323d3c709
DMITRIY\adich <adichev17@gmail.com>
2023-10-09 00:36:38 +0300

    feat: :heavy_plus_sign: add health-check for auth service

--- Microservices/Authentication/AuthenticationApiService/Program.cs
+++ Microservices/Authentication/AuthenticationApiService/Program.cs
@@ -2,8 +2,10 @@ using Authentication.Application;
 using Authentication.Infrastructure;
 using AuthenticationApiService.Errors;
 using AuthenticationApiService.Mapping;
+using HealthChecks.UI.Client;
 using JwtAuthenticationManager;
 using MessageBus;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,11 +25,30 @@ builder.Services.AddMessageBus();
 builder.Services.AddJwtAuthentication();
 builder.Services.AddJwtManagerDependency();
 
+// Health checks
+builder.Services
+    .AddHealthChecks()
+    .AddSqlServer(builder.Configuration.GetConnectionString("AuthDatabase"));
+builder.Services.AddHealthChecksUI(setupSettings: setup =>
+{
+    setup.SetEvaluationTimeInSeconds(15);
+    setup.AddHealthCheckEndpoint("auth-service", "/healthz"); //map health check api
+}).AddInMemoryStorage();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseRouting();
+// Health Checks
+app.UseRouting().UseEndpoints(config =>
+{
+    config.MapHealthChecks(
+        "/healthz",
+        new HealthCheckOptions
+        {
+            ResponseWriter =
+                UIResponseWriter.WriteHealthCheckUIResponse
+        });
+    config.MapHealthChecksUI(x => x.UIPath = "/health-dashboard");
+});
 app.UseExceptionHandler("/error");
 app.UseAuthentication();
 app.UseAuthorization();
